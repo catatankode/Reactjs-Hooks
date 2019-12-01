138d89f00ec547ae6781b8424e84627100ec3eed
rendiwijiatmoko <rendiwijiatmoko97@gmail.com>
2019-12-01 11:47:05 +0700

    useState, delete file app.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, {useEffect} from 'react' // import useEffect
 
 const App = () => {
+  // useEffect pertama
   useEffect(() => {
     const clicked = () => console.log('window clicked')
     window.addEventListener('click', clicked)
 
-    // return a clean-up function / unmount
     return () => {
       window.removeEventListener('click', clicked)
     }
   }, [])
 
-  return (
+  // useEffect kedua
+  useEffect(() => {
+    console.log("another useEffect call");
+  })
+
+  return (  
     <div>
-      klik dimana aja akan memberikan pesan di console 
+      Untuk melihat hasilnya buka console di browser
     </div>
-  )
+  ) 
 }
 
 export default App;
